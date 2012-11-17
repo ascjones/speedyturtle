1149d55cf6a8b1053a84da05f3d04bad97a70ca1
Andrew Jones <ascjones@gmail.com>
2012-11-17 09:49:45 +0000

    can now submit a task and view its details

---
+++ SpeedyTurtle/Controllers/TaskController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SpeedyTurtle.Models;
+
+namespace SpeedyTurtle.Controllers
+{
+    public class TaskController : RavenController
+    {
+        public ActionResult Submit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit(TurtleTask turtleTask)
+        {
+            RavenSession.Store(turtleTask);
+            return RedirectToAction("Details", new {id = turtleTask.Id});
+        }
+
+        public ActionResult Details(int id)
+        {
+            var task = RavenSession.Load<TurtleTask>(id);
+            return View(task);
+        }
+    }
+}
